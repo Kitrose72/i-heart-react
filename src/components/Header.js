45eae2e4887de30ee94398908a6baa37fe7780da
Kit <kitrose72@gmail.com>
2022-04-02 11:37:25 -0400

    w1 workshop

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import '../App.css';
+
+const Header = () => {
+  return (
+    <div className='App-header'>I ❤️ React</div>
+  );
+}
+
+function App(props) {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
